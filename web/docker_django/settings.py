d6b76aab95e62690f8a8553df56a20e6ec887bda
Michael Herman <hermanmu@gmail.com>
2016-01-10 14:53:21 -0700

    updated settings.py with the STATIC_ROOT

--- web/docker_django/settings.py
+++ web/docker_django/settings.py
@@ -112,7 +112,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-print(STATICFILES_DIRS)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+# print(STATICFILES_DIRS)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
