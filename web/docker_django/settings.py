07c1a46785364e5be052857119841fd01cdd6e45
Michael Herman <hermanmu@gmail.com>
2016-05-08 09:14:29 -0700

    updated allowed_hosts

--- web/docker_django/settings.py
+++ web/docker_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
