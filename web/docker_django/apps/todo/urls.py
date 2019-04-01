d3dc0dd9d2450f51c75337083edcdd4597f4ec1d
Florian Dahlitz <f2dahlitz@freenet.de>
2019-04-01 22:40:53 +0200

    feat(web): update to Python 3.7 and latest package versions

--- web/docker_django/apps/todo/urls.py
+++ web/docker_django/apps/todo/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
+    re_path(r'^$', views.home, name='home'),
 ]
